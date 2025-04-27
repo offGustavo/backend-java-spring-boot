de4576ede7a5da3cde08c402e043053427d59c82
Luiz Gustavo <luizgmdcm@gmail.com>
2025-04-27 04:26:30 -0300

    ADD: add webConfig, and enable cross comunnication

---
+++ src/main/java/com/turboorder/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.turboorder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    // Permitir requisições de localhost:3000 (ou qualquer origem do seu frontend)
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000") // Altere o endereço para o seu frontend
+        .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+        .allowedHeaders("*") // Permite todos os cabeçalhos
+        .allowCredentials(true); // Permite credenciais, como cookies
+  }
+}
